ea1806e13e2616980858c2f4b5c95a3c9da778db
TobisawaMisakii <cpy040407@gmail.com>
2025-04-25 16:04:42 +0800

    bug fixed: var_decl & var_def node relationship

--- tests/test2-1.c
+++ tests/test2-1.c
@@ -1,22 +1,8 @@
 int main()
 {
 	int a, c;
+    int e = 1, f;
     int b;
-    a = 3;
-    b = 2;
-    {
-        int c;
-        c = a + b;
-        b = c;
-	}
-
-	{
-        int a, b;
-        a = 4;
-        b = 5;
-        c = a + b;
-
-	}
-
-	return a + b + c;
+    int d = 2;
+    return 0;
 }
